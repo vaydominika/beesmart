267f331ee3dafab088c25926122e220211799143
vaydominika <vaydominika@gmail.com>
2026-02-17 09:00:01 +0100

    refactor: Replaced Lucide Search icon with Hugeicons Search01Icon and updated input styling.

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -1,34 +1,35 @@
 "use client";
 
-import { Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { MainContent } from "@/components/dashboard/MainContent";
 import { WelcomeBanner } from "@/components/dashboard/WelcomeBanner";
 import { BasedOnYourCoursesCard } from "@/components/dashboard/BasedOnYourCoursesCard";
 import { SurpriseMeCard } from "@/components/dashboard/SurpriseMeCard";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { HugeiconsIcon } from '@hugeicons/react';
+import { Search01Icon } from '@hugeicons/core-free-icons';
 
 export default function DashboardPage() {
   return (
     <ScrollArea className="h-full overflow-y-auto">
-    <div className="flex flex-col">
-      <div className="px-6 pt-4 pb-6 space-y-4">
-        <div className="relative">
-          <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-(--theme-text)" />
-          <Input
-            type="search"
-            placeholder="SEARCH"
-            className="pl-10 bg-white border-gray-200 text-(--theme-text) w-full"
-          />
+      <div className="flex flex-col">
+        <div className="px-6 pt-4 pb-6 space-y-4">
+          <div className="relative">
+            <HugeiconsIcon icon={Search01Icon} strokeWidth={2.5} className="absolute left-3 top-1.5 h-5 w-5 text-(--theme-text)" />
+            <Input
+              type="search"
+              placeholder="SEARCH"
+              className="pl-9 bg-white border-(--theme-card) text-(--theme-text) w-full rounded-xl corner-squircle"
+            />
+          </div>
+          <WelcomeBanner />
+          <div className="flex flex-col md:flex-row gap-4">
+            <BasedOnYourCoursesCard />
+            <SurpriseMeCard />
+          </div>
         </div>
-        <WelcomeBanner />
-        <div className="flex flex-col md:flex-row gap-4">
-          <BasedOnYourCoursesCard />
-          <SurpriseMeCard />
-        </div>
+        <MainContent />
       </div>
-      <MainContent />
-    </div>
     </ScrollArea>
   );
 }
